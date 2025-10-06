0517ff97b5a53db8b066f809a41335de86e54882
Rishit999 <rishitnayakar@gmail.com>
2025-10-06 15:35:56 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,6 +1,11 @@
+def compound_interest(prin, rate, time, n):
+    amount = principal * (1 + rate / n) ** (n * time)
+    interest = amount - principal
+    return amount, interest
 prin = int(input("Enter the Principle Amount"))
 rate = float(input("Enter the Rate of Interest"))
 time = int(input("Enter the Time Period"))
+n = 12
 
 si = (prin * time * rate)/100
 
