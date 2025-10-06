a607b17dde8442288d38ed07fa766fe3b3eb1d78
Rishit999 <rishitnayakar@gmail.com>
2025-10-06 15:31:50 +0530

    Create si.py

---
+++ si.py
@@ -0,0 +1,7 @@
+prin = int(input("Enter the Principle Amount"))
+rate = float(input("Enter the Rate of Interest"))
+time = int(input("Enter the Time Period"))
+
+si = (prin * time * rate)/100
+
+print(f"Simple Interest is {si}")
