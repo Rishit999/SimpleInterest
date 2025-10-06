3411aadec6c4c24b57ed7dd0c3773ad7feb3bddb
Rishit999 <rishitnayakar@gmail.com>
2025-10-06 15:53:06 +0530

    Update si.py

--- si.py
+++ si.py
@@ -1,10 +1,10 @@
 prin = int(input("Enter the Principle Amount"))
 rate = float(input("Enter the Rate of Interest"))
 time = int(input("Enter the Time Period"))
-n = 12
+
 
 si = (prin * time * rate)/100
-ci = p * ((1 + r/100) ** t) - p
+ci = prin * ((1 + rate/100) ** time) - prin
 
 print(f"Simple Interest is {si:.2f}")
 print(f"Compound Intrest is {ci:.2f}")
